8dec48fefa73d00f86d71ad02296e1ed2ac5564e
Daniel Strebel <daniel.strebel@gmail.com>
2021-12-26 15:16:51 +0100

    day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -5,7 +5,7 @@ import (
 	"fmt"
 )
 
-func SimulateLatnernFishPopulation(initialPopulation []int, daysSimulation int) []int {
+func SimulateLatnernFishPopulation(initialPopulation []int, daysSimulation int) int {
 	simulatedPopulation := initialPopulation
 	for day := 1; day <= daysSimulation; day++ {
 		newFish := []int{}
@@ -19,11 +19,35 @@ func SimulateLatnernFishPopulation(initialPopulation []int, daysSimulation int) 
 		}
 		simulatedPopulation = append(simulatedPopulation, newFish...)
 	}
-	return simulatedPopulation
+	return len(simulatedPopulation)
+}
+
+func CompactSimulateLatnernFishPopulation(initialPopulation []int, daysSimulation int) uint64 {
+	simulatedPopulation := make([]uint64, 9)
+	for _, p := range initialPopulation {
+		simulatedPopulation[p] = simulatedPopulation[p] + 1
+	}
+	for day := 1; day <= daysSimulation; day++ {
+		newFish := simulatedPopulation[0]
+		for i := 0; i < 8; i++ {
+			simulatedPopulation[i] = simulatedPopulation[i+1]
+		}
+		simulatedPopulation[8] = newFish
+		simulatedPopulation[6] = simulatedPopulation[6] + newFish
+	}
+
+	populationSize := uint64(0)
+	for _, size := range simulatedPopulation {
+		populationSize += size
+	}
+	return populationSize
 }
 
 func PrintSolution() {
 	initalPopulation := utils.ParseIntArrays(utils.ParseLines("./inputs/day6.txt"), ",")
 	simulatedPopulation := SimulateLatnernFishPopulation(initalPopulation[0], 80)
-	fmt.Println("Simulated Population Size (Part 1)", len(simulatedPopulation))
+	fmt.Println("Simulated Population Size after 80 days(Part 1)", simulatedPopulation)
+	initalPopulation = utils.ParseIntArrays(utils.ParseLines("./inputs/day6.txt"), ",")
+	largeSimulatedPopulation := CompactSimulateLatnernFishPopulation(initalPopulation[0], 256)
+	fmt.Println("Simulated Population Size after 256 (Part 2)", largeSimulatedPopulation)
 }
--- day6/day6_test.go
+++ day6/day6_test.go
@@ -7,7 +7,25 @@ func TestLanternFishPopulation(t *testing.T) {
 
 	population := SimulateLatnernFishPopulation(input, 18)
 	expectedPopulationSize := 26
-	if len(population) != expectedPopulationSize {
-		t.Error("invalid simulation population size. Got", len(population), "expected", expectedPopulationSize)
+	if population != expectedPopulationSize {
+		t.Error("invalid simulation population size after 18 days. Got", population, "expected", expectedPopulationSize)
 	}
+
+}
+
+func TestCompactLanternFishPopulation(t *testing.T) {
+	input := []int{3, 4, 3, 1, 2}
+
+	population := CompactSimulateLatnernFishPopulation(input, 18)
+	expectedPopulationSize := uint64(26)
+	if population != expectedPopulationSize {
+		t.Error("invalid simulation population size after 18 days. Got", population, "expected", expectedPopulationSize)
+	}
+
+	population256 := CompactSimulateLatnernFishPopulation(input, 256)
+	expectedPopulationSize256 := uint64(26984457539)
+	if population256 != expectedPopulationSize256 {
+		t.Error("invalid simulation population size after 256 days. Got", population256, "expected", expectedPopulationSize256)
+	}
+
 }
