72005002b3b1ed2ec7146aa218b51bd6597c9e06
Daniel Strebel <daniel.strebel@gmail.com>
2021-12-28 13:43:52 +0100

    day 7 test

--- day7/day7.go
+++ day7/day7.go
@@ -33,12 +33,24 @@ func OptimalCrabsPositionTwo(initalPositions []int) int {
 	for _, p := range initalPositions {
 		sum += p
 	}
-	mean := math.Floor(float64(sum) / float64(len(initalPositions)))
+	meanFloor := math.Floor(float64(sum) / float64(len(initalPositions)))
+	meanCeil := math.Ceil(float64(sum) / float64(len(initalPositions)))
 
+	fuelFloor := calcFuel(initalPositions, meanFloor)
+	fuelCeil := calcFuel(initalPositions, meanCeil)
+
+	if fuelFloor < fuelCeil {
+		return fuelFloor
+	} else {
+		return fuelCeil
+	}
+}
+
+func calcFuel(positions []int, center float64) int {
 	fuel := 0
 
-	for _, p := range initalPositions {
-		delta := int(math.Abs(float64(p) - mean))
+	for _, p := range positions {
+		delta := int(math.Abs(float64(p) - center))
 		fuelP := (delta * (delta + 1)) / 2
 		fuel += fuelP
 	}
