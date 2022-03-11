1c1c39fd3498711b08a6b2b83ae5eb295f251dee
Daniel Strebel <daniel.strebel@gmail.com>
2022-03-11 10:38:57 +0100

    day 18 explode and split

--- day18/day18.go
+++ day18/day18.go
@@ -1,6 +1,10 @@
 package day18
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"strconv"
+)
 
 type TNode struct {
 	parent *TNode
@@ -9,12 +13,11 @@ type TNode struct {
 	value  int
 }
 
-func parseTNode(input string) *TNode {
+func parseTNode(input string) TNode {
 	var currentNode *TNode
 	currentNode = &TNode{nil, nil, nil, -1}
 
 	for i := 0; i < len(input); i++ {
-		fmt.Print(string(input[i]))
 		switch input[i] {
 		case '[':
 			{
@@ -31,12 +34,143 @@ func parseTNode(input string) *TNode {
 		case ']':
 			currentNode = currentNode.parent
 		default:
-			currentNode.value = int(input[i] - '0')
+			numberLength := 1
+			value := -1
+			for {
+				parsed, err := strconv.ParseInt(input[i:i+numberLength], 10, 64)
+				if err != nil {
+					break
+				} else {
+					value = int(parsed)
+					numberLength++
+				}
+			}
+			i += (numberLength - 2)
+			fmt.Println(value)
+			currentNode.value = value
 		}
 
 	}
 
-	return currentNode
+	return *currentNode
+}
+
+func findNextLeft(t *TNode) *TNode {
+	currentNode := t
+	for currentNode.parent != nil {
+		if currentNode.parent.right == currentNode {
+			return currentNode.parent.left
+		}
+		currentNode = currentNode.parent
+	}
+	return nil
+}
+
+func findNextRight(t *TNode) *TNode {
+	currentNode := t
+	for currentNode.parent != nil {
+		if currentNode.parent.left == currentNode {
+			currentNode = currentNode.parent.right
+			for currentNode != nil && currentNode.left != nil {
+				currentNode = currentNode.left
+			}
+			return currentNode
+		}
+		currentNode = currentNode.parent
+	}
+	return nil
+}
+
+func isNumberPair(t TNode) bool {
+	return t.left != nil && t.left.value >= 0 && t.right != nil && t.right.value >= 0
+}
+
+func explode(t TNode) TNode {
+	root := &t
+	currentNode := root
+	currentDepth := 0
+
+	for {
+		if currentDepth >= 4 && isNumberPair(*currentNode) {
+			//explode
+			leftValue := currentNode.left.value
+			rightValue := currentNode.right.value
+
+			currentNode.left = nil
+			currentNode.right = nil
+			currentNode.value = 0
+
+			// add left number to next left
+			if nextLeft := findNextLeft(currentNode); nextLeft != nil {
+				nextLeft.value += leftValue
+			}
+
+			// add right number to next left
+			if nextRight := findNextRight(currentNode); nextRight != nil {
+				nextRight.value += rightValue
+			}
+			break
+		} else if currentNode.left != nil {
+			currentDepth += 1
+			currentNode = currentNode.left
+		} else if currentNode.right != nil {
+			currentDepth += 1
+			currentNode = currentNode.right
+		} else {
+			for currentNode.parent != nil {
+				if currentNode.parent.left == currentNode {
+					currentNode = currentNode.parent.right
+					break
+				}
+				currentDepth -= 1
+				currentNode = currentNode.parent
+			}
+
+			if currentNode.parent == nil {
+				break //back at root
+			}
+		}
+	}
+	return *root
+}
+
+func split(t TNode) TNode {
+	root := &t
+	currentNode := root
+	fmt.Println(currentNode)
+
+	for {
+		if currentNode.value >= 10 {
+			//explode
+			leftValue := int(math.Floor(float64(currentNode.value) / 2.0))
+			rightValue := int(math.Ceil(float64(currentNode.value) / 2.0))
+
+			currentNode.left = &TNode{currentNode, nil, nil, leftValue}
+			currentNode.right = &TNode{currentNode, nil, nil, rightValue}
+			currentNode.value = -1
+
+			fmt.Println(currentNode)
+
+			break
+		} else if currentNode.left != nil {
+			currentNode = currentNode.left
+		} else if currentNode.right != nil {
+			currentNode = currentNode.right
+		} else {
+			for currentNode.parent != nil {
+				if currentNode.parent.left == currentNode {
+					currentNode = currentNode.parent.right
+					break
+				}
+				currentNode = currentNode.parent
+			}
+
+			if currentNode.parent == nil {
+				break //back at root
+			}
+		}
+	}
+	return *root
 }
 
 func (tn TNode) String() string {
--- day18/day18_test.go
+++ day18/day18_test.go
@@ -21,3 +21,63 @@ func TestNodeParser(t *testing.T) {
 		}
 	}
 }
+
+func TestExplode(t *testing.T) {
+	tests := [][]string{
+		{
+			"[[[[[9,8],1],2],3],4]",
+			"[[[[0,9],2],3],4]",
+		},
+		{
+			"[7,[6,[5,[4,[3,2]]]]]",
+			"[7,[6,[5,[7,0]]]]",
+		},
+		{
+			"[[6,[5,[4,[3,2]]]],1]",
+			"[[6,[5,[7,0]]],3]",
+		},
+		{
+			"[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]",
+			"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+		},
+		{
+			"[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]",
+			"[[3,[2,[8,0]]],[9,[5,[7,0]]]]",
+		},
+	}
+
+	for _, test := range tests {
+		input := test[0]
+		expected := test[1]
+
+		explodedOut := explode(parseTNode(input)).String()
+
+		if explodedOut != expected {
+			t.Error("Unexpected explode output. Expected:", expected, "got", explodedOut)
+		}
+	}
+}
+
+func TestSplit(t *testing.T) {
+	tests := [][]string{
+		{
+			"[[[[0,7],4],[15,[0,13]]],[1,1]]",
+			"[[[[0,7],4],[[7,8],[0,13]]],[1,1]]",
+		},
+		{
+			"[[[[0,7],4],[[7,8],[0,13]]],[1,1]]",
+			"[[[[0,7],4],[[7,8],[0,[6,7]]]],[1,1]]",
+		},
+	}
+
+	for _, test := range tests {
+		input := test[0]
+		expected := test[1]
+
+		splitOut := split(parseTNode(input)).String()
+
+		if splitOut != expected {
+			t.Error("Unexpected split output. Expected:", expected, "got", splitOut)
+		}
+	}
+}
